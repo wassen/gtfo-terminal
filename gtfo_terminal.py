47ba8dfb91a945cb6560647db266334bf84274f9
wassen <k-asayama@outlook.com>
2020-11-29 15:28:47 +0900

    feat: helpコマンド

--- gtfo_terminal.py
+++ gtfo_terminal.py
@@ -2,24 +2,55 @@
 # -*- coding:utf-8 -*-
 
 import discord
+from typing import Optional
 
-client = discord.Client()
+def read_token() -> str:
+    with open('token') as token_file:
+        return token_file.read()
 
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+def read_help() -> str:
+    with open('help') as help_file:
+        return help_file.read()
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+class Command:
+    def output(self) -> str:
+        pass
 
-with open('token') as token_file:
-    token = token_file.read()
+class Help(Command):
+    def output(self) -> str:
+        return read_help()
 
-client.run(token)
+class UnknownCommand:
+    def output(self) -> Optional[str]:
+        pass
 
+def parse(message_content: str) -> Command:
+    elements: list[str] = message_content.split(" ")
+
+    command: str = elements[0].lower()
+
+    if "help".startswith(command):
+        return Commands()
+    else:
+        return UnknownCommand()
+
+class GTFOTerminal(discord.Client):
+    async def on_ready(self):
+        print(f'We have logged in as {self.user}'.format())
+
+    async def on_message(self: discord.Client, message: discord.Message):
+        if message.author == self.user:
+            return
+
+        if not message.channel.name == 'gtfo_playing':
+            return
+
+        command = parse(message.content)
+
+        output: Optional[str] = command.output()
+        if command.output() is not None:
+            await message.channel.send(output)
+
+GTFOTerminal().run(read_token())
 
